74706cbb2afd9841c68fd95872dc40c240a30c69
silb90 <31510932+silb90@users.noreply.github.com>
2020-03-12 14:23:59 +0100

    Adding new form for registering with e-mail and password

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -3,16 +3,16 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Container } from 'reactstrap';
 import './LoginPage.css';
 
 export class LoginPage extends Component{
     render(){
-        return(
-            <Row>
-                <Col xs={12} md={4}>
+        return (
 
-                </Col>
-                <Col xs={12} md={4}>
+            <table className='table'>
+            <Row>
+                <Col>
                     <Form>
                         <h1 className='loginh1'>Log in</h1>
                         <Form.Group controlId="formBasicEmail">
@@ -35,10 +35,35 @@ export class LoginPage extends Component{
                         </Button>
                     </Form>
                 </Col>
-                <Col xs={12} md={4}>
-                    
+                <Col>
+                    <Form>
+
+                        <h1 className='loginh1'>Register</h1>
+                        
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" size="lg">
+                            Submit
+                        </Button>
+                    </Form>
                 </Col>
-            </Row>
+
+             </Row>
+
+            </table>
         );
     }
 }
