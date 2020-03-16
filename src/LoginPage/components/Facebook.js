1adc641f8b9c80a6ddc872ba77016e3d37167d89
silb90 <31510932+silb90@users.noreply.github.com>
2020-03-16 13:19:21 +0100

    Uniformized code for fb and google login, fixed button for google

--- src/LoginPage/components/Facebook.js
+++ src/LoginPage/components/Facebook.js
@@ -1,4 +1,34 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import '../LoginPage.css';
+import FacebookLogin from 'react-facebook-login';
+
+class Facebook extends Component {
+
+    render() {
+  
+      const responseFacebook = (response) => {
+        console.log(response);
+      }
+  
+      return (
+        <div className="Facebook">
+  
+        <FacebookLogin
+          appId="1315775898620150"
+          fields="name,email,picture"
+          callback={responseFacebook}
+        />
+  
+        </div>
+      );
+    }
+  }
+  
+  export default Facebook;
+
+
+
+/*import React, {Component} from 'react'
 import FacebookLogin from 'react-facebook-login'
 
 
@@ -53,4 +83,4 @@ export default class Facebook extends Component {
         <div> {fbContent}</div>
         )
     }
-}
+}*/
--- src/LoginPage/components/Gmail.js
+++ src/LoginPage/components/Gmail.js
@@ -1,91 +1,27 @@
-import React, {Component} from 'react'
-import {ReactDOM} from 'react-dom';
-//import {GoogleLogin} from 'react-google-login';
-import {GoogleLogout} from 'react-google-login';
-import { GoogleLogin } from 'react-google-login-component';
-import GoogleButton from 'react-google-button';
+import React, { Component } from 'react';
+import '../LoginPage.css';
+import GoogleLogin from 'react-google-login';
 
-class Gmail extends Component{
- 
-  constructor (props, context) {
-    super(props, context);
-  }
- 
-  responseGoogle (googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    var googleId = googleUser.getId();
-    
-    console.log({ googleId });
-    console.log({accessToken: id_token});
-    //anything else you want to do(save to localStorage)...
-  }
+class Gmail extends Component {
 
-  
- 
-  render () {
+  render() {
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
     return (
-      <div>
-        <GoogleLogin className = 'customBtn' socialId="903553020968-hias98fifmunqoeq3o4da5jj8sjgl53v.apps.googleusercontent.com"
-                     className="google-login"
-                     scope="profile"
-                     fetchBasicProfile={false}
-                     responseHandler={this.responseGoogle}
-                     buttonText="Login With Google"/>
+      <div className="Gmail">
+        <GoogleLogin
+          clientId="903553020968-hias98fifmunqoeq3o4da5jj8sjgl53v.apps.googleusercontent.com"
+          buttonText="Sign in with google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
 
-    <GoogleButton
-                onClick={() => { console.log('Google button clicked') 
-            }}
-            ></GoogleButton>
       </div>
-
-            /*<GoogleLogout
-            clientId="903553020968-hias98fifmunqoeq3o4da5jj8sjgl53v.apps.googleusercontent.com"
-            buttonText="Logout"
-            onLogoutSuccess={logout}
-            >
-            </GoogleLogout>*/
     );
   }
 }
- 
+
 export default Gmail;
-/*export default class Facebook extends Component {
-
-    state = {
-        isLoggedIn: false,
-        userID: '',
-        name: '',
-        email: '',
-        picture: ''
-    }
-
-    responseGoogle = response => {
-    //console.log(response)
-        this.setState({
-            isLoggedIn: true,
-            userID: response.userID,
-            name: response.name,
-            email: response.email,
-            picture: response.picture.data.url
-    })
-}
-
-ReactDOM.render(
-    <div className="Gmail">
-         <GoogleLogin>
-            clientId="903553020968-hias98fifmunqoeq3o4da5jj8sjgl53v.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            isSignedIn={true}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-         </GoogleLogin>
-
-     
-    </div>
-)
-}*/
-
-
-  
-
