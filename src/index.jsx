e095113a456b203e8e4551a3a32df5090247e085
shvalade <volodymyr.shakhrai@gmail.com>
2020-04-15 19:49:23 +0300

    add deploy fix4

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+//testing
 
 
 ReactDOM.render(<BrowserRouter basename="http://localhost:3000"><App /> </BrowserRouter>, document.getElementById('root'));
