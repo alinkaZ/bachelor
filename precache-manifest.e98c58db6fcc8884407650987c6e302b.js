626a136109f0c6e7e5b5475db3fe8e15a5248adc
shvalade <volodymyr.shakhrai@gmail.com>
2020-04-15 19:39:18 +0300

    Updates

---
+++ precache-manifest.e98c58db6fcc8884407650987c6e302b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933feead5a6d8d77ff54a48c76d2c8b9",
+    "url": "/bachelor/index.html"
+  },
+  {
+    "revision": "f2a24622618efc74932e",
+    "url": "/bachelor/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b9174a072263e1aac1c8",
+    "url": "/bachelor/static/css/main.7273b8c4.chunk.css"
+  },
+  {
+    "revision": "f2a24622618efc74932e",
+    "url": "/bachelor/static/js/2.e8df8971.chunk.js"
+  },
+  {
+    "revision": "f0384b182436f8023ed53fa8abb349d8",
+    "url": "/bachelor/static/js/2.e8df8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9174a072263e1aac1c8",
+    "url": "/bachelor/static/js/main.d4ebb236.chunk.js"
+  },
+  {
+    "revision": "041bafe9c49cf94716dc",
+    "url": "/bachelor/static/js/runtime-main.ebfeae4b.js"
+  },
+  {
+    "revision": "3d3747f536e4f3afd6d9004fbb505354",
+    "url": "/bachelor/static/media/coffee.3d3747f5.jpg"
+  },
+  {
+    "revision": "c1847251307407ae216edb8d9888e0e8",
+    "url": "/bachelor/static/media/image.c1847251.png"
+  },
+  {
+    "revision": "618a58be54b18cdf3457582ea29b82a5",
+    "url": "/bachelor/static/media/plus.618a58be.png"
+  }
+]);
