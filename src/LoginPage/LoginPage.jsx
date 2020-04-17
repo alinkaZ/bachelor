388b59885b605f9192de48c85fd76d81518c3d60
alinkaZ <43778396+alinkaZ@users.noreply.github.com>
2020-04-17 19:47:35 +0200

    from bachelor2

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,92 +1,73 @@
-import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Container } from 'reactstrap';
-import './LoginPage.css';
-import Facebook from './components/Facebook';
-import Gmail from './components/Google'
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Container } from "reactstrap";
+import "./LoginPage.css";
+import Facebook from "./components/Facebook";
+import Gmail from "./components/Google";
 
-export class LoginPage extends Component{
-    render(){
-        return (
+export class LoginPage extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xm={12} xs={4}></Col>
+          <Col xm={12} xs={4}>
+            <Form>
+              <h1 className="loginh1">Log in</h1>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-            <table className='table'>
-            <Row>
-                <Col>
-                    <Form>
-                        <h1 className='loginh1'>Log in</h1>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" size="lg" href="/admin">
-                            Submit
-                        </Button>
-                    </Form>
-                </Col>
-                <Col>
-                    <Form>
-
-                        <h1 className='loginh1'>Register</h1>
-                        
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" size="lg">
-                            Submit
-                        </Button>
-                    </Form>
-                </Col>
-
-             </Row>
-             <Row>
-                 <Col></Col>
-                 <Col><h2>Alternatives:</h2>
-                 </Col>
-                 <Col></Col>
-                
-             </Row>
-             <Row>
-                <Col>
-                    <div className ="Facebook">
-                        <Facebook />
-                    </div>
-                </Col>
-                <Col>
-                    <div className ="Google">
-                        <Gmail/>
-                    </div>
-                </Col>
-                <Col>
-                </Col>
-             </Row>
-
-            </table>
-        );
-    }
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Row>
+                <Button variant="primary" type="submit" size="lg" href="/admin">
+                  Submit
+                </Button>
+                <Button
+                  variant="primary"
+                  type="register"
+                  size="lg"
+                  href="/admin"
+                >
+                  Register
+                </Button>
+              </Row>
+            </Form>
+          </Col>
+          <Col xm={12} xs={4}></Col>
+        </Row>
+        <Row>
+          <Col xm={12} xs={4}></Col>
+          <Col xm={12} xs={4} className="facebook">
+            <hr />
+            <p>or signup with</p>
+            <hr />
+          </Col>
+          <Col xm={12} xs={4}></Col>
+        </Row>
+        <Row>
+          <Col xxm={12} xs={4}></Col>
+          <Col xm={12} xs={4}>
+            <div className="facebook">
+              <Facebook />
+            </div>
+          </Col>
+          <Col xm={12} xs={4}></Col>
+        </Row>
+      </Container>
+    );
+  }
 }
--- src/ModulPage/ModulDetailPageVideo/Video.jsx
+++ src/ModulPage/ModulDetailPageVideo/Video.jsx
@@ -8,9 +8,9 @@ export class Video extends Component{
     render(){
         return(
                 <div style={{ width: 660, height: 'auto'}}>
-                    <ResponsiveEmbed aspectRatio="16by9">
-                        <embed type="image/svg+xml" src="https://www.youtube.com/watch?v=ia1iuXbEaYQ" />
-                    </ResponsiveEmbed>
+                   
+                        <img  />
+                   
                 </div>    
         );
     }
